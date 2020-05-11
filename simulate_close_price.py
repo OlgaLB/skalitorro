95aca1c9285f50ce2ce3c14db923922d6b14318a
OlgaLB <60224128+OlgaLB@users.noreply.github.com>
2020-05-11 15:42:49 +0200

    Update simulate_close_price.py

--- simulate_close_price.py
+++ simulate_close_price.py
@@ -8,7 +8,7 @@ import os
 
 # get historic data for March 2019
 historic_march_data = yf.download("^RUI", start="2019-03-01", end="2019-03-31")
-print('Historic March data: ' + str(historic_march_data))
+print('Historic March data: {}'.format(historic_march_data))
 
 if not os.path.exists('charts'):
     os.makedirs('charts')
@@ -26,7 +26,7 @@ reg.fit(X, y)
 
 # get historic data for April 2019
 historic_april_data = yf.download("^RUI", start="2019-04-01", end="2019-04-30")
-print('Historic April data: ' + str(historic_april_data))
+print('Historic April data: {}'.format(historic_april_data))
 
 # build chart for historic data for April 2019
 historic_april_data['Close'].plot()
@@ -37,8 +37,8 @@ predicted_close_prices = []
 for index, row in historic_april_data.iterrows():
     predicted_price = reg.predict([[row['Open'], row['High'], row['Low']]])
     predicted_close_prices.append(predicted_price)
-    print('Predicted April Price: ' + str(predicted_price))
-    print('Real April Price: ' + str(row[['Close']]))
+    print('Predicted April Price: {}'.format(predicted_price))
+    print('Real April Price: '.format(row[['Close']]))
 
 # build chart for predicted data for April 2019
 plt.plot(range(len(predicted_close_prices)), predicted_close_prices)
